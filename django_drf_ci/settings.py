d20df0b210f6b96de55197949b93b78bc8e70c13
aikchun <aikchun616@gmail.com>
2017-06-08 13:39:12 +0800

    Added staticfiles for gitignore configure static root for static files collection

--- django_drf_ci/settings.py
+++ django_drf_ci/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -129,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
